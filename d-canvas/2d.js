40efaf4d3b2cc8701969f24cf43377191411c886
Mitsuru Oshima <oshima@chromium.org>
2020-04-20 09:28:38 -0700

    fix transform

--- d-canvas/2d.js
+++ d-canvas/2d.js
@@ -33,19 +33,19 @@ function init2D() {
 	break;
       case 90:
 	canvas.style.transformOrigin = "top left";
-	canvas.style.left = `${dp_height}px`;
+	canvas.style.left = `${dp_width}px`;
 	canvas.style.top = "0px";
 	console.log('90 = ' + `${dp_height}`);
 	break;
       case 180:
 	canvas.style.transformOrigin = "top left";
-	canvas.style.left = `${dp_width}px`;
-	canvas.style.top = `${dp_height}px`;
+	canvas.style.left = `${dp_height}px`;
+	canvas.style.top = `${dp_width}px`;
 	break;
       case 270:
 	canvas.style.transformOrigin = "top left";
 	canvas.style.left = "0px";
-	canvas.style.top = `${dp_width}px`;
+	canvas.style.top = `${dp_height}px`;
 	break;
     }
 
