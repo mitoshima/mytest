4cfe5d101b1b58ba82f7dcbcc1fdeb01d80a5acf
Mitsuru Oshima <oshima@chromium.org>
2022-07-25 14:27:58 -0700

    FIx

--- d-canvas/webgl.js
+++ d-canvas/webgl.js
@@ -1,3 +1,8 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
 function initWebGL() {
   const canvas = document.querySelector('canvas');
   const gl = canvas.getContext('webgl', {desynchronized: true, alpha: false});
@@ -10,9 +15,9 @@ function initWebGL() {
   let hue = 0;
   const raf = () => {
     hue += 1;
-    r = Math.sin(0.05 * hue + 0) * 127 + 128;
-    g = Math.sin(0.05 * hue + 2) * 127 + 128;
-    b = Math.sin(0.05 * hue + 4) * 127 + 128;
+    const r = Math.sin(0.05 * hue + 0) * 127 + 128;
+    const g = Math.sin(0.05 * hue + 2) * 127 + 128;
+    const b = Math.sin(0.05 * hue + 4) * 127 + 128;
     gl.clearColor(r / 255, g / 255, b / 255, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.flush();
@@ -24,18 +29,17 @@ function initWebGL() {
     const angle = screen.orientation.angle % 360;
     canvas.style.transform = `rotateZ(${angle}deg)`;
     const dpr = devicePixelRatio;
-    //      canvas.style.border = `${1 / dpr}px solid black`;
 
-    var dp_width = window.innerWidth;
-    var dp_height = window.innerHeight;
-    var pixel_width = Math.floor(dp_width * dpr);
-    var pixel_height = Math.floor(dp_height * dpr);
+    const dp_width = window.innerWidth;
+    const dp_height = window.innerHeight;
+    const pixel_width = Math.floor(dp_width * dpr);
+    const pixel_height = Math.floor(dp_height * dpr);
     console.log('update:' + angle + ", size=" + dp_width + "x" + dp_height);
 
     if (angle % 180 == 90) {
       canvas.style.width = `${dp_height}px`;
       canvas.style.height = `${dp_width}px`;
-      var offset = (dp_height - dp_width) / 2;
+      const offset = (dp_height - dp_width) / 2;
       canvas.style.left = `-${offset}px`;
       canvas.style.top = `${offset}px`;
       canvas.height = pixel_width;
@@ -48,30 +52,12 @@ function initWebGL() {
       canvas.width  = pixel_width;
       canvas.height = pixel_height;
     }
-    /*
-      c2.fillStyle = 'rgb(255,255,0)';
-      c2.fillRect(0, 0, pixel_width, pixel_height);
-      c2.strokeStyle = 'rgb(255,0,0)';
-      c2.strokeRect(0, 0, pixel_width, pixel_height);
-
-      c2.fillStyle = 'rgb(255,255,255)';
-      c2.fillRect(100, 100, 200, 200);
-
-      // Text
-      c2.fillStyle = 'rgb(255,255,255)';
-      c2.font = "40px Arial";
-      c2.fillText(`pixel size=${pixel_width}x${pixel_height} dp size=${dp_width}x${dp_height} dpr=${dpr}`, 10, 50);
-      c2.strokeStyle = 'rgb(0,0,0)';
-      c2.strokeText(`pixel size=${pixel_width}x${pixel_height} dp size=${dp_width}x${dp_height} dpr=${dpr}`, 10, 50);
-*/
 
     gl.disable(gl.SCISSOR_TEST);
     gl.clearColor(1, 1, 0.2, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.flush();
     gl.scissor(125, 63, 250, 125);
-    //angle % 180 == 90 ? gl.scissor(63, 125, 125, 250)
-    //    : gl.scissor(125, 63, 250, 125);
     gl.enable(gl.SCISSOR_TEST);
   };
   screen.orientation.addEventListener('change', setSizeAndRotation);
